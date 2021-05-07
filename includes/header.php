975b67c64229bb0fe409f9f977837a3c64780c54
Patricia-Nalwanga <pmubiru@gmail.com>
2021-05-07 15:47:32 +0300

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+
+
+<div id="centrecontainer">
+  <div id="outercontainer">
+    <div id="container">
+      <div id="headercont">
+        <ul>
+          <li><a href="#">valid xhtml</a></li>
+          <li>|</li>
+          <li><a href="#">valid css</a></li>
+        </ul>
+      </div>
+      <div id="maincont">
+        <div id="mainleft">
